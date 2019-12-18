aca2cd58678b059f5169737528d8915a2433b878
Kamil Mendelski <kamil@frontlabs.pl>
2019-12-18 13:29:16 +0100

    Removed some of the obsolete Sapper template files

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -37,10 +37,3 @@
 </svelte:head>
 
 <h1>Great success!</h1>
-
-<figure>
-	<img alt='Borat' src='great-success.png'>
-	<figcaption>HIGH FIVE!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
