5dbb63aef4173eec98d6863eee5be4366348fb79
Kamil Mendelski <kamil@frontlabs.pl>
2020-01-17 17:04:04 +0100

    Rewrite of the structure to enable scroll spy in future

---
+++ src/components/Contact.svelte
@@ -0,0 +1,57 @@
+<script>
+    import ContactForm from '../components/ContactForm.svelte';
+</script>
+
+<div class="scene contact">
+    <div class="text">
+        <div class="text__inside">
+            <h2 class="title">
+                Let's connect!
+            </h2>
+            <div class="lead">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing  elit. Porttitor odio sed eros adipiscing commodo. Tempor semper orci, fames neque.</p>
+            </div>
+
+            <ContactForm />
+        </div>
+    </div>
+</div>
+
+<style>
+.contact {
+    display: flex;
+    align-items: flex-start;
+    flex-direction: column;
+    position: relative;
+    background: none;
+    z-index: 6;
+    height: 80vh;
+}
+
+.contact::after {
+    content: "";
+    display: block;
+    height: 50vh;
+    width: 100%;
+    background-color: #ccf06b;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    z-index: -1;
+}
+
+.contact .text {
+    top: 5vh;
+    justify-content: flex-end;
+}
+
+.contact .text__inside {
+    width: 28vw;
+    margin-right: 21vw;
+}
+
+.contact .lead {
+    margin-bottom: 4.5vh;
+}
+</style>
---
+++ src/components/Energy.svelte
@@ -0,0 +1,35 @@
+<div class="scene energy">
+    <div class="text">
+        <div class="container">
+            <div class="text__inside">
+                <p class="subtitle"><strong>Gaia Green Tech</strong> is</p>
+                <h2 class="title">
+                    Clean<br>
+                    energy supply
+                </h2>
+                <div class="lead">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing  elit. Porttitor odio sed eros adipiscing commodo. Tempor semper orci, fames neque.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+.energy {
+    display: flex;
+    align-items: flex-start;
+    flex-direction: column;
+    position: relative;
+    background: none;
+    z-index: 5;
+}
+
+.energy .text {
+    top: 18.7vh;
+}
+
+.energy .text .container {
+    margin-left: 11vw;
+}
+</style>
