c84b14b1adaf9765a6e20f9a136dc1db92c64b9d
Wojtek Siudzinski <admin@suda.pl>
2021-09-29 15:22:36 +0200

    Refactor base wraper as a separate component

---
+++ src/components/BaseWrapper.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let onScroll;
+</script>
+
+<div on:scroll={onScroll} class="base-wrapper">
+  <slot />
+</div>
+
+<style>
+  .base-wrapper {
+    height: 100vh;
+    overflow-x: hidden;
+    overflow-y: auto;
+    perspective: 3px;
+    position: relative;
+  }
+</style>
