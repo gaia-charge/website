a618ce6b27c9050871aad1964179aaf992e02562
Wojtek Siudzinski <admin@suda.pl>
2021-10-11 19:15:38 +0200

    Add Sentry

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -17,6 +17,14 @@
   import { afterUpdate, beforeUpdate } from "svelte";
   import { useAckeeSapper } from "svelte-ackee";
   import { startClient } from "../services/i18n";
+  import * as Sentry from "@sentry/browser";
+  import { Integrations } from "@sentry/tracing";
+
+  Sentry.init({
+    dsn: "https://3e6147df857f4f798b79196e963e1895@o1035330.ingest.sentry.io/6002173",
+    integrations: [new Integrations.BrowserTracing()],
+    tracesSampleRate: 1.0,
+  });
 
   export let initialLocale;
   startClient(initialLocale);
