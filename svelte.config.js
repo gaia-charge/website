ae09f8322e2990ebe930a631af81854522312997
Wojtek Siudzinski <admin@suda.pl>
2022-01-27 10:48:44 +0000

    Add attractions, chartsjs and other deps

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,17 @@
 /** @type {import('@sveltejs/kit').Config} */
 
 import adapter from "@sveltejs/adapter-static";
+import preprocess from 'svelte-preprocess';
+import makeAttractionsImporter from 'attractions/importer.js';
 
 const config = {
+  preprocess: preprocess({
+    scss: {
+      importer: makeAttractionsImporter({
+        themeFile: 'static/attractions-theme.scss',
+      }),
+    },
+  }),
   kit: {
     adapter: adapter(),
   },
