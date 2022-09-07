23c72221c368145d22fdb62ccc3427910ed97842
Wojtek Siudzinski <admin@suda.pl>
2022-09-07 12:35:30 +0200

    Fix the devalue issue https://github.com/sveltejs/kit/issues/6462

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,8 @@ import { sveltekit } from "@sveltejs/kit/vite";
 /** @type {import('vite').UserConfig} */
 const config = {
   plugins: [sveltekit()],
+  ssr: {
+    noExternal: ["devalue"],
+  },
 };
 export default config;
