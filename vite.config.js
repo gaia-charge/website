610599606c7ee365a22fa10808cd76f987427a33
Wojtek Siudzinski <admin@suda.pl>
2022-09-07 12:19:28 +0200

    Update SvelteKit to 1.0.0-next.475

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,5 @@ import { sveltekit } from "@sveltejs/kit/vite";
 /** @type {import('vite').UserConfig} */
 const config = {
   plugins: [sveltekit()],
-  ssr: {
-    noExternal: ["devalue"],
-  },
 };
 export default config;
