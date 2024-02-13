edf6dde1bd3d2ce95f3b61f647aace2421fad928
Wojtek Siudzinski <admin@suda.pl>
2024-02-13 21:58:20 +0100

    Use static $env

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,7 +1,7 @@
 import { sequence } from "@sveltejs/kit/hooks";
 import { handleErrorWithSentry, sentryHandle } from "@sentry/sveltekit";
 import * as Sentry from '@sentry/sveltekit';
-import { VITE_SENTRY_DSN } from "$env/dynamic/private";
+import { VITE_SENTRY_DSN } from "$env/static/private";
 
 Sentry.init({
   dsn: VITE_SENTRY_DSN,
