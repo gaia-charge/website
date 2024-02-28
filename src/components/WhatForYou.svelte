3be8cf4556be84090b517c56bc10e096d4084a8b
gennaro.martone <ge.martone@gmail.com>
2024-02-28 23:12:49 +0100

    what for you completed

--- src/components/WhatForYou.svelte
+++ src/components/WhatForYou.svelte
@@ -1,36 +1,27 @@
 <script>
   import { _, isLoading } from "svelte-i18n";
-
-  import planning from "$lib/assets/svg/planning.svg";
-  import line1 from "$lib/assets/svg/line1.svg";
-  import installation from "$lib/assets/svg/installation.svg";
-  import line2 from "$lib/assets/svg/line2.svg";
-  import monitoring from "$lib/assets/svg/monitoring.svg";
-  import line3 from "$lib/assets/svg/line3.svg";
-  import maintenance from "$lib/assets/svg/maintenance.svg";
-  import line4 from "$lib/assets/svg/line4.svg";
-  import payments from "$lib/assets/svg/payments.svg";
   import arrow_right from "$lib/assets/svg/arrow_right.svg";
+  import green_arrow_right from "$lib/assets/svg/green_arrow_right.svg"
 </script>
 
 {#if !$isLoading}
 <section>
   <div class="section-evidence bg-medium-blue text-white flex flex-col">
-    <div><p>{$_("whatWeDo.evidence.uptitle", {
+    <div><p>{$_("whatForYou.evidence.uptitle", {
       default: "Cobramos una tarifa fija mensual por cargador",
     })}</p>
     </div>
-    <h2>{$_("whatWeDo.evidence.title", {
+    <h2>{$_("whatForYou.evidence.title", {
       default: "¡Para que sólo te centres en tu negocio!",
     })}</h2>
     <div class="flex flex-row gap-8 items-center">
-      <p>{$_("whatWeDo.evidence.p1", {
+      <p>{$_("whatForYou.evidence.p1", {
         default: "Sin inversión inicial",
       })}</p>
-      <span class="divider px-8 pr-8"><p>{$_("whatWeDo.evidence.p2", {
+      <span class="divider px-8 pr-8"><p>{$_("whatForYou.evidence.p2", {
         default: "Tú eliges si quieres ofrecer carga gratuita o de pago",
       })}</p></span>
-      <p>{$_("whatWeDo.evidence.p3", {
+      <p>{$_("whatForYou.evidence.p3", {
         default: "Sin letra pequeña",
       })}</p>
     </div>
@@ -39,12 +30,99 @@
         type="submit"
         class="contact ml-auto text-white bg-green border-green border-2 rounded-full flex items-center"
       >
-      {$_("whatWeDo.evidence.cta", {
+      {$_("whatForYou.evidence.cta", {
         default: "Contacto",
       })}
         <img src={arrow_right} class="inline-block" alt="Contacto" />
       </button>
     </div>
+  </div>
+  <div class="for-you">
+    <div class="container">
+      <div class="title text-center mx-auto mb-10">
+        <h1>{$_("whatWeDo.title", {
+          default: "¿Por qué te beneficias al trabajar con Gaia?",
+        })}</h1>
+      </div>
+      <div class=" grid grid-cols-3 gap-16">
+        <!-- 1 Card -->
+        <div class="card">
+          <div class="title">
+            <h3>{$_("whatWeDo.card1.title1", {
+              default: "Haz foco",
+            })}</h3></div>
+          <div class="title flex gap-2 items-center">
+            <img src={green_arrow_right} class="inline-block fill-green" alt="To" />
+            <h3>{$_("whatWeDo.card1.title2", {
+              default: "en tu negocio",
+            })}</h3></div>
+          <p class=" mt-6">{$_("whatWeDo.card1.description", {
+            default: "Porque al no tener que ocuparte de nada mientras trabajemos juntos, puedes seguir haciendo foco en tu negocio",
+          })}</p>
+        </div>
+        <!-- 2 Card -->
+        <div class="card">
+          <div class="title">
+            <h3>{$_("whatWeDo.card1.title1", {
+              default: "Haz foco",
+            })}</h3></div>
+          <div class="title flex gap-2 items-center">
+            <img src={green_arrow_right} class="inline-block fill-green" alt="To" />
+            <h3>{$_("whatWeDo.card1.title2", {
+              default: "en tu negocio",
+            })}</h3></div>
+          <p class=" mt-6">{$_("whatWeDo.card1.description", {
+            default: "Porque al no tener que ocuparte de nada mientras trabajemos juntos, puedes seguir haciendo foco en tu negocio",
+          })}</p>
+        </div>
+        <!-- 3 Card -->
+        <div class="card">
+          <div class="title">
+            <h3>{$_("whatWeDo.card1.title1", {
+              default: "Haz foco",
+            })}</h3></div>
+          <div class="title flex gap-2 items-center">
+            <img src={green_arrow_right} class="inline-block fill-green" alt="To" />
+            <h3>{$_("whatWeDo.card1.title2", {
+              default: "en tu negocio",
+            })}</h3></div>
+          <p class=" mt-6">{$_("whatWeDo.card1.description", {
+            default: "Porque al no tener que ocuparte de nada mientras trabajemos juntos, puedes seguir haciendo foco en tu negocio",
+          })}</p>
+        </div>
+        <!-- 4 Card -->
+        <div class="card">
+          <div class="title">
+            <h3>{$_("whatWeDo.card1.title1", {
+              default: "Haz foco",
+            })}</h3></div>
+          <div class="title flex gap-2 items-center">
+            <img src={green_arrow_right} class="inline-block fill-green" alt="To" />
+            <h3>{$_("whatWeDo.card1.title2", {
+              default: "en tu negocio",
+            })}</h3></div>
+          <p class=" mt-6">{$_("whatWeDo.card1.description", {
+            default: "Porque al no tener que ocuparte de nada mientras trabajemos juntos, puedes seguir haciendo foco en tu negocio",
+          })}</p>
+        </div>
+        <!-- 5 Card -->
+        <div class="card">
+          <div class="title">
+            <h3>{$_("whatWeDo.card1.title1", {
+              default: "Haz foco",
+            })}</h3></div>
+          <div class="title flex gap-2 items-center">
+            <img src={green_arrow_right} class="inline-block fill-green" alt="To" />
+            <h3>{$_("whatWeDo.card1.title2", {
+              default: "en tu negocio",
+            })}</h3></div>
+          <p class=" mt-6">{$_("whatWeDo.card1.description", {
+            default: "Porque al no tener que ocuparte de nada mientras trabajemos juntos, puedes seguir haciendo foco en tu negocio",
+          })}</p>
+        </div>
+      </div>
+    </div>
+
   </div>
 </section>
 {/if}
@@ -52,6 +130,46 @@
 <style lang="postcss">
   :root {
     --ratio: 1440 * 1vw * 100;
+  }
+
+  .card{
+    max-width: 280px;
+  }
+  .card .title h3 {
+    font-family: theme("fontFamily.serif");
+    font-size: calc(32 / var(--ratio));
+    line-height: calc(40 / var(--ratio));
+    font-weight: 500;
+  }
+
+  .card p {
+    font-family: theme("fontFamily.serif");
+    font-size: calc(18 / var(--ratio));
+    line-height: calc(28 / var(--ratio));
+    font-weight: 400;
+  }
+
+  .for-you{
+    background-color: white;
+    
+  }
+  .for-you .container{
+    max-width: calc( 1008 / var(--ratio));
+    margin-inline-start: auto;
+    margin-inline-end: auto;
+    padding-block-start: calc( 32 / var(--ratio));
+    padding-block-end: calc( 53 / var(--ratio));
+  }
+  .for-you .title{
+    width: calc(540 / var(--ratio));
+    
+  }
+
+  .for-you .title h1 {
+    font-family: theme("fontFamily.serif");
+    font-size: calc(48 / var(--ratio));
+    line-height: calc(56 / var(--ratio));
+    font-weight: 400;
   }
   .submit {
     width: auto;
