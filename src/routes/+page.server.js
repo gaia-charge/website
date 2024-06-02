2819e9c2e0787a8d4644b95b74bb86df5c72c817
Wojtek Siudzinski <admin@suda.pl>
2024-06-02 21:56:48 +0200

    Fix the contact form

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,18 +1,18 @@
 import { redirect } from '@sveltejs/kit';
-import {match} from '@formatjs/intl-localematcher';
+import { match } from '@formatjs/intl-localematcher';
 import Negotiator from 'negotiator';
 
-const supportedLocales = ['en','es'];
+const supportedLocales = ['en', 'es'];
 const defaultLocale = 'en';
 
 /** @type {import('./$types').LayoutServerLoad} */
-export const load = async ({cookies, request}) => {
+export const load = async ({ cookies, request }) => {
 	const res = match(
 		new Negotiator({
-			headers: {'accept-language': request.headers.get('accept-language') || ''},
+			headers: { 'accept-language': request.headers.get('accept-language') || '' },
 		}).languages(),
 		[...supportedLocales],
 		defaultLocale
 	)
-	redirect(307, '/'+res);
+	redirect(307, '/' + res);
 }
--- src/routes/[slug]/+page.server.js
+++ src/routes/[slug]/+page.server.js
@@ -2,40 +2,40 @@ import { env } from '$env/dynamic/private'
 
 
 // export async function load() {
-  // const url = new URL(
-  //   `https://api.hubapi.com/forms/v2/forms/${HUBSPOT_FORM_ID}`
-  // );
-  // const request = new Request(url, {
-  //   method: "GET",
-  //   headers: {
-  //     "cache-control": "max-age=3600", // cache form response for 1 hour
-  //     Authorization: `Bearer ${HUBSPOT_PRIVATE_APP_KEY}`,
-  //   },
-  // });
+// const url = new URL(
+//   `https://api.hubapi.com/forms/v2/forms/${HUBSPOT_FORM_ID}`
+// );
+// const request = new Request(url, {
+//   method: "GET",
+//   headers: {
+//     "cache-control": "max-age=3600", // cache form response for 1 hour
+//     Authorization: `Bearer ${HUBSPOT_PRIVATE_APP_KEY}`,
+//   },
+// });
 
-  // const response = await fetch(request);
-  // const data = await response.json();
+// const response = await fetch(request);
+// const data = await response.json();
 
 //   // return data;
 //   return {};
 // }
 
-import {match} from '@formatjs/intl-localematcher';
+import { match } from '@formatjs/intl-localematcher';
 import Negotiator from 'negotiator';
 
-const supportedLocales = ['en','es'];
+const supportedLocales = ['en', 'es'];
 const defaultLocale = 'en';
 
 /** @type {import('./$types').LayoutServerLoad} */
-export const load = async ({cookies, request}) => {
-	const res = match(
-		new Negotiator({
-			headers: {'accept-language': request.headers.get('accept-language') || ''},
-		}).languages(),
-		[...supportedLocales],
-		defaultLocale
-	)
-	//throw redirect(307, '/'+res);
+export const load = async ({ cookies, request }) => {
+  const res = match(
+    new Negotiator({
+      headers: { 'accept-language': request.headers.get('accept-language') || '' },
+    }).languages(),
+    [...supportedLocales],
+    defaultLocale
+  )
+  //throw redirect(307, '/'+res);
 }
 
 export const actions = {
@@ -49,30 +49,32 @@ export const actions = {
         objectTypeId: '0-1'
       });
     }
-    // const result = await fetch(
-    //   `https://api.hsforms.com/submissions/v3/integration/submit/${env.HUBSPOT_PORTAL_ID}/${env.HUBSPOT_FORM_ID}`,
-    //   {
-    //     method: "POST",
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //       Authorization: `Bearer ${env.HUBSPOT_PRIVATE_APP_KEY}`,
-    //     },
-    //     body: JSON.stringify({data}),
-    //   }
-    // );
-    const url = new URL(
-      `https://api.hubapi.com/forms/v2/forms/${env.HUBSPOT_FORM_ID}`
+    const response = await fetch(
+      `https://api.hsforms.com/submissions/v3/integration/submit/${env.HUBSPOT_PORTAL_ID}/${env.HUBSPOT_FORM_ID}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${env.HUBSPOT_PRIVATE_APP_KEY}`,
+        },
+        body: JSON.stringify({ fields }),
+      }
     );
-    const requestHS = new Request(url, {
-      method: "GET",
-      headers: {
-        "cache-control": "max-age=3600", // cache form response for 1 hour
-        Authorization: `Bearer ${env.HUBSPOT_PRIVATE_APP_KEY}`,
-      },
-    });
+    console.log(response);
+    // const url = new URL(
+    //   `https://api.hubapi.com/forms/v2/forms/${env.HUBSPOT_FORM_ID}`
+    // );
+    // const requestHS = new Request(url, {
+    //   method: "GET",
+    //   headers: {
+    //     "cache-control": "max-age=3600", // cache form response for 1 hour
+    //     Authorization: `Bearer ${env.HUBSPOT_PRIVATE_APP_KEY}`,
+    //   },
+    // });
 
-    const response = await fetch(requestHS);
+    // const response = await fetch(requestHS);
     const result = await response.json();
+    console.log(JSON.stringify(result, null, 2));
     let contactStatus = "thank-you";
 
     if (result.status > 201) {
