d1129bbe6bcf1750d14990a5651952c3fa6cd0de
Kamil Mendelski <kamil@frontlabs.pl>
2024-06-20 00:14:57 +0200

    fix(nav): Added variable to control menu

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -7,6 +7,7 @@
   import Hamburger from "./menu/Hamburger.svelte";
 
   export let sidebar = false;
+  export let showMobileMenu = false;
 </script>
 
 <nav
@@ -19,10 +20,13 @@
       <a href="/"><img src={logo} class="logo" alt="Gaia Charge logo" /></a>
     </div>
     <div class="flex md:hidden mobile-view_p">
-      <!-- <a href="/"><img src={menuMobile} class="menu" alt="Mobile Menu" /></a> -->
+      {#if showMobileMenu}
+        <a href="/"><img src={menuMobile} class="menu" alt="Mobile Menu" /></a>
+      {/if}
+
       <Hamburger bind:open={sidebar} />
     </div>
-    <div class=" hidden md:flex justify-center gap-4">
+    <div class="hidden md:flex justify-center gap-4">
       <a href="#need" class="navigation"
         >{$_("nav.need", {
           default: "Why you need it",
@@ -49,15 +53,15 @@
         })}</a
       >
     </div>
-    <div class=" hidden md:flex">
+    <div class="hidden md:flex">
       <a href="#contact" class="contact text-green border-green rounded-full"
         >{$_("contactBtn.cta", {
           default: "Contacto",
         })}</a
       >
     </div>
-    <div class=" hidden md:flex">
-      <p class=" w-12">
+    <div class="hidden md:flex">
+      <p class="w-12">
         <select class="select w-full" bind:value={$locale}>
           {#each $locales as locale}
             <option value={locale}>{locale}</option>
@@ -69,13 +73,6 @@
 </nav>
 
 <style lang="postcss">
-  :root {
-    --ratio: 1440 * 1vw * 100;
-  }
-  select:focus-visible {
-    outline: none;
-  }
-
   .select {
     -webkit-appearance: none;
     -moz-appearance: none;
@@ -88,6 +85,11 @@
     font-weight: 700;
     line-height: 28px;
   }
+
+  select:focus-visible {
+    outline: none;
+  }
+
   nav {
     top: 0;
     right: 0;
@@ -111,22 +113,24 @@
     color: #044348;
     padding: calc(8 / var(--ratio));
   }
+
   .logo {
     width: calc(105 / var(--ratio));
+    min-width: 87px;
   }
+
   .menu {
     width: calc(55 / var(--ratio));
   }
+
   .contact {
     font-size: calc(16 / var(--ratio));
     font-weight: 400;
     padding: calc(15 / var(--ratio));
     border-width: calc(2 / var(--ratio));
   }
+
   @media only screen and (max-width: 431px) {
-    .logo {
-      width: 87px;
-    }
     nav {
       padding-block-start: 16px;
       padding-right: 0 !important;
