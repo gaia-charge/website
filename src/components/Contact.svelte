758bba55d25ecd9419802a307e440a4f7c848b4e
Kamil Mendelski <kamil@frontlabs.pl>
2024-10-21 15:22:46 +0200

    feat(pipedrive): cleanup of obsolete events

--- src/components/Contact.svelte
+++ src/components/Contact.svelte
@@ -1,29 +1,9 @@
 <script>
   import { onMount } from "svelte";
   import { _, isLoading } from "svelte-i18n";
-  import arrow_right from "$lib/assets/svg/arrow_right.svg";
-  import schema from "./../schema";
 
-  export let contactStatus;
-
-  let values = {};
-  let errors = {};
-  let formBind;
-
-  const verify = async () => {
-    try {
-      await schema.validate(values, { abortEarly: false });
-      errors = {};
-      formBind.submit();
-    } catch (err) {
-      errors = err.inner.reduce((acc, err) => {
-        return { ...acc, [err.path]: err.message };
-      }, {});
-    }
-  };
-
-  const scriptId = 'pipedriveWebForms';
-  const loadPipedriveScript = () => {
+  onMount(() => {
+    const scriptId = 'pipedriveWebForms';
     const scriptElem = document.getElementById(scriptId);
 
     if (scriptElem) {
@@ -34,52 +14,31 @@
     script.setAttribute('id', scriptId);
     script.setAttribute('src','https://webforms.pipedrive.com/f/loader');
     document.head.appendChild(script);
-  }
-
-  onMount(async () => {
-    loadPipedriveScript();
   })
 </script>
 
 {#if !$isLoading}
   <div id="contact"></div>
   <div class="contact bg-medium-blue text-white flex flex-row">
-    {#if contactStatus}
-      <div class=" flex p-0 info w-full h-96 whitespace-pre-line">
-        <h2 class="whitespace-pre-line m-auto">
-          {#if contactStatus !== "error"}
-            {$_("contact.thankYouMsg", {
-              default:
-                "Thank you for contacting us.\nWe will respond to you shortly",
-            })}
-          {:else}
-            {$_("contact.msgError", {
-              default: "There was an error.\n Please retry later",
-            })}
-          {/if}
-        </h2>
-      </div>
-    {:else}
-      <div class="info w-1/2 mt-6">
-        <h2>
-          {@html $_("contact.title", {
-            default: "We are here for you",
-          })}
-        </h2>
-        <p>
-          {$_("contact.subtitle", {
-            default:
-              "Leave us your contact details and we will get in touch with you as soon as possible.",
-          })}
-        </p>
-      </div>
-      <div class="w-1/2 form-elements">
-        <div
-          class="pipedriveWebForms"
-          data-pd-webforms="https://webforms.pipedrive.com/f/72moX2d3I5kKo6qKLYRPmCBRuSlQfrEVbV9IYYbZ8qljei7JSW8hIcBKlnfvgqgMGD"
-        />
-      </div>
-    {/if}
+    <div class="info w-1/2 mt-6">
+      <h2>
+        {@html $_("contact.title", {
+          default: "We are here for you",
+        })}
+      </h2>
+      <p>
+        {$_("contact.subtitle", {
+          default:
+            "Leave us your contact details and we will get in touch with you as soon as possible.",
+        })}
+      </p>
+    </div>
+    <div class="w-1/2 form-elements">
+      <div
+        class="pipedriveWebForms"
+        data-pd-webforms="https://webforms.pipedrive.com/f/72moX2d3I5kKo6qKLYRPmCBRuSlQfrEVbV9IYYbZ8qljei7JSW8hIcBKlnfvgqgMGD"
+      />
+    </div>
   </div>
 {/if}
 
--- src/routes/[slug]/+page.server.js
+++ src/routes/[slug]/+page.server.js
@@ -1,25 +1,4 @@
 import { env } from '$env/dynamic/private'
-
-
-// export async function load() {
-// const url = new URL(
-//   `https://api.hubapi.com/forms/v2/forms/${HUBSPOT_FORM_ID}`
-// );
-// const request = new Request(url, {
-//   method: "GET",
-//   headers: {
-//     "cache-control": "max-age=3600", // cache form response for 1 hour
-//     Authorization: `Bearer ${HUBSPOT_PRIVATE_APP_KEY}`,
-//   },
-// });
-
-// const response = await fetch(request);
-// const data = await response.json();
-
-//   // return data;
-//   return {};
-// }
-
 import { match } from '@formatjs/intl-localematcher';
 import Negotiator from 'negotiator';
 
@@ -37,50 +16,3 @@ export const load = async ({ cookies, request }) => {
   )
   //throw redirect(307, '/'+res);
 }
-
-export const actions = {
-  submit: async ({ request }) => {
-    const data = await request.formData();
-    const fields = [];
-    for (const el of data) {
-      fields.push({
-        name: el[0],
-        value: el[1],
-        objectTypeId: '0-1'
-      });
-    }
-    const response = await fetch(
-      `https://api.hsforms.com/submissions/v3/integration/submit/${env.HUBSPOT_PORTAL_ID}/${env.HUBSPOT_FORM_ID}`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${env.HUBSPOT_PRIVATE_APP_KEY}`,
-        },
-        body: JSON.stringify({ fields }),
-      }
-    );
-    // const url = new URL(
-    //   `https://api.hubapi.com/forms/v2/forms/${env.HUBSPOT_FORM_ID}`
-    // );
-    // const requestHS = new Request(url, {
-    //   method: "GET",
-    //   headers: {
-    //     "cache-control": "max-age=3600", // cache form response for 1 hour
-    //     Authorization: `Bearer ${env.HUBSPOT_PRIVATE_APP_KEY}`,
-    //   },
-    // });
-
-    // const response = await fetch(requestHS);
-    const result = await response.json();
-    let contactStatus = "thank-you";
-
-    if (result.status > 201) {
-      contactStatus = "error";
-    }
-
-    return {
-      contactStatus,
-    };
-  },
-};
