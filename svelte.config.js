903274cd82d7640142e4a0101a1319562f7c264e
Kamil Mendelski <kamil@frontlabs.pl>
2024-10-21 13:58:05 +0200

    chore(config): enable customElement in compiler option

--- svelte.config.js
+++ svelte.config.js
@@ -2,11 +2,13 @@
 
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-
 const config = {
   kit: {
     adapter: adapter()
   },
-  preprocess: vitePreprocess()
+  preprocess: vitePreprocess(),
+  compilerOptions: {
+    customElement: true,
+  }
 };
 export default config;
